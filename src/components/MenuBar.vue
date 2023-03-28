e73069ffdba44b645f1314d958f1d4ec7b6d4b67
raphael meyer <raphael.akira.meyer@gmail.com>
2023-03-28 22:12:41 +0200

    Add character icon

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Character } from '@/domain/character'
+import deathwalkerIcon from '@/assets/any2cards/fh-deathwalker-bw-icon.png'
 
 defineProps<{ character: Character }>()
 </script>
@@ -7,8 +8,10 @@ defineProps<{ character: Character }>()
 <template>
   <v-app-bar color="grey-darken-3">
     <v-app-bar-title>
-      <v-icon icon="mdi-account"></v-icon>
-      <span> {{ character.name }}</span>
+      <v-avatar color="grey-lighten-3">
+        <v-img :src="deathwalkerIcon" max-height="1.5em" max-width="1.5em"></v-img>
+      </v-avatar>
+      <span class="ml-4">{{ character.name }}</span>
     </v-app-bar-title>
   </v-app-bar>
 </template>
