14f8187564700d3c962e3db2a106b7e54c0afab6
Raphael Meyer <raphael.akira.meyer@gmail.com>
2023-04-23 17:17:39 +0200

    Improve perk text rendering

--- src/components/character-sheet/PerkText.vue
+++ src/components/character-sheet/PerkText.vue
@@ -1,21 +1,42 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-
 import { modifiers } from '@/domain/icons';
 import type { Modifier } from '@/domain/modifiers';
+import { computed } from 'vue';
 
 const props = defineProps<{ text: string }>();
 
-const tokens = computed(() => {
+type Icon = { type: 'icon'; source: string };
+type Text = { type: 'text'; words: string[] };
+type Token = Icon | Text;
+
+const tokens = computed((): Token[] => {
   const reSplit = /(<[\w-]+>)/;
   const reIcon = /<([\w-]+)>/;
-  return props.text.split(reSplit).map((token) => {
-    const icon = token.match(reIcon)?.at(1);
+
+  const empty = (text: string[]) => text.length === 0 || text.every((word) => word === '');
+
+  const createIcon = (part: string) => {
+    const icon = part.match(reIcon)?.at(1);
     if (icon !== undefined && icon in modifiers) {
-      return { type: 'icon', value: modifiers[icon as Modifier] };
+      return modifiers[icon as Modifier];
     }
-    return { type: 'text', value: token };
-  });
+    return undefined;
+  };
+
+  return props.text
+    .split(reSplit)
+    .map((part) => {
+      const icon = createIcon(part);
+      if (icon !== undefined) {
+        const result: Icon = { type: 'icon', source: icon };
+        return result;
+      }
+
+      const words = part.trim().split(' ');
+      const result: Text = { type: 'text', words };
+      return result;
+    })
+    .filter((token) => !(token.type === 'text' && empty(token.words)));
 });
 </script>
 
@@ -23,10 +44,18 @@ const tokens = computed(() => {
   <div class="d-flex flex-wrap align-center">
     <template v-for="(token, i) in tokens" :key="i">
       <template v-if="token.type === 'icon'">
-        <v-img :src="token.value" max-width="1.2em" max-height="1.2em" class="mr-1"> </v-img>
+        <v-img
+          :src="token.source"
+          min-width="1em"
+          min-height="1em"
+          max-width="1.2em"
+          max-height="1.2em"
+          class="mr-1"
+        >
+        </v-img>
       </template>
       <template v-if="token.type === 'text'">
-        <span class="mr-1"> {{ token.value }} </span>
+        <span v-for="(word, w) in token.words" :key="w" class="mr-1"> {{ word }} </span>
       </template>
     </template>
   </div>
