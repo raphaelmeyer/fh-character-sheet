fe2906ff4933c75e50f70a55e1f2bca439a4ed2c
raphael meyer <raphael.akira.meyer@gmail.com>
2023-04-23 21:27:57 +0200

    Use local storage for mercenaries

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+
+import { useMercenaryStore } from '@/stores/mercenary';
+import { onMounted } from 'vue';
+
+const store = useMercenaryStore();
+
+store.$onAction((action) => {
+  action.after(() => {
+    localStorage.setItem('mercenaries', JSON.stringify(store.mercenaries));
+  });
+});
+
+onMounted(() => {
+  const stored = localStorage.getItem('mercenaries');
+  if (stored) {
+    const mercenaries = JSON.parse(stored);
+    store.mercenaries = mercenaries;
+  }
+});
 </script>
 
 <template>
   <RouterView />
 </template>
-
-<style scoped></style>
